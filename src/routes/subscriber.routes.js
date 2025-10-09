bd395824fb51ba0b1465a560d9480bc4eeac5c95
mohd-faisal07 <faisalahmed36950@gmail.com>
2025-10-09 17:13:19 +0530

    added searching fro subscribers in BE

--- src/routes/subscriber.routes.js
+++ src/routes/subscriber.routes.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const subscriberController = require("../controllers/subscriber.controller");
 
+
+router.get('/search', subscriberController.searchSubscribers);
+router.get('/suggestions', subscriberController.getSubscriberSuggestions);
+router.get('/:id/details', subscriberController.getSubscriberDetails);
+
 router.post("/", subscriberController.createSubscriber); // create
 router.get("/", subscriberController.getAllSubscribers); // read all
 router.get("/:id", subscriberController.getSubscriberById); // read one
